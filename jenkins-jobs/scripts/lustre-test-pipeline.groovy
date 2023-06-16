4ec2ed067e82feaf711002fecff00682e05ed46d
Xinliang Liu <xinliang.liu@linaro.org>
2023-06-16 03:22:24 +0000

    Templating the lustre-periodical-test job
    In template, curly brace should be doubled.
    
    Signed-off-by: Xinliang Liu <xinliang.liu@linaro.org>

--- jenkins-jobs/scripts/lustre-test-pipeline.groovy
+++ jenkins-jobs/scripts/lustre-test-pipeline.groovy
@@ -1,81 +1,80 @@
-def maloolink="https://testing.whamcloud.com/test_sessions?jobs=custom&user_id=b8340029-197d-4ce0-a8f1-40f76d3bb8c7&builds=${BUILD_ID}#redirect"
+def maloolink="https://testing.whamcloud.com/test_sessions?jobs=custom&user_id=b8340029-197d-4ce0-a8f1-40f76d3bb8c7&builds=${{BUILD_ID}}#redirect"
 def buildlink="http://213.146.155.72:8080/job/project-lustre-build-release-master/lastBuild/"
 
-pipeline {
-    agent {
+pipeline {{
+    agent {{
         label 'lustre-test-agent'
-    }
+    }}
 
-    environment {
+    environment {{
         CUMULATIVE_RESULT_ID = UUID.randomUUID().toString()
         LUSTRE_BRANCH = "master"
-    }
+    }}
 
-    options {
+    options {{
         timeout(time: 24, unit: 'HOURS')
-    }
+    }}
 
-    stages {
-        stage('Cleanup workspace') {
-            steps {
-                sh 'ls -l ${WORKSPACE}'
+    stages {{
+        stage('Cleanup workspace') {{
+            steps {{
+                sh 'ls -l ${{WORKSPACE}}'
                 sh 'rm -fr *'
-                sh 'ln -sf /home/centos/workspace/nfs/test_logs ${WORKSPACE}/test_logs'
-            }
-        }
+                sh 'ln -sf /home/centos/workspace/nfs/test_logs ${{WORKSPACE}}/test_logs'
+            }}
+        }}
 
-        stage('Build') {
-            steps {
+        stage('Build') {{
+            steps {{
                 echo 'Building..'
-                build job: 'project-lustre-build-release-master',
-                      parameters: [string(name: 'BRANCH', value: "${LUSTRE_BRANCH}")]
-            }
-        }
+                build job: 'project-lustre-build-release-{distro_branch}'
+	    }}
+        }}
 
-        stage('Download scripts') {
-            steps {
+        stage('Download scripts') {{
+            steps {{
                 sh 'wget -c https://github.com/Linaro/lustretest/archive/refs/heads/main.zip'
                 sh 'unzip main.zip'
-            }
-        }
+            }}
+        }}
 
-        stage('Test Execution'){
-            parallel {
-                stage('Run test group 1') {
-                    steps {
-                        dir("lustretest-main/lustretest") {
-                            sh 'source /home/centos/venv3/bin/activate;python3 test_runner.py 1 false'
-                        }
-                    }
-                }
-                stage('Run test group 2') {
-                    steps {
-                        dir("lustretest-main/lustretest") {
-                            sh 'source /home/centos/venv3/bin/activate;python3 test_runner.py 2 false'
-                        }
-                    }
-                }
-                stage('Run test group 3') {
-                    steps {
-                        dir("lustretest-main/lustretest") {
-                            sh 'source /home/centos/venv3/bin/activate;python3 test_runner.py 3 false'
-                        }
-                    }
-                }
-            }
-        }
-    }
+        stage('Test Execution'){{
+            parallel {{
+                stage('Run test group 1') {{
+                    steps {{
+                        dir("lustretest-main/lustretest") {{
+                            sh 'source /home/centos/venv3/bin/activate;python test_runner.py --test-group-id 1 '
+                        }}
+                    }}
+                }}
+                stage('Run test group 2') {{
+                    steps {{
+                        dir("lustretest-main/lustretest") {{
+                            sh 'source /home/centos/venv3/bin/activate;python test_runner.py --test-group-id 2'
+                        }}
+                    }}
+                }}
+                stage('Run test group 3') {{
+                    steps {{
+                        dir("lustretest-main/lustretest") {{
+                            sh 'source /home/centos/venv3/bin/activate;python test_runner.py --test-group-id 3'
+                        }}
+                    }}
+                }}
+            }}
+        }}
+    }}
 
-    post {
-        always {
-            dir("lustretest-main/lustretest") {
-                sh 'source /home/centos/venv3/bin/activate;python3 upload_results.py'
-            }
-            script {
+    post {{
+        always {{
+            dir("lustretest-main/lustretest") {{
+                //sh 'source /home/centos/venv3/bin/activate;python3 upload_results.py'
+            }}
+            script {{
                 def summary = manager.createSummary("package.gif")
-                summary.appendText("See <a href=${buildlink}>build job.</a>", false)
-                summary.appendText("<br>See <a href=${maloolink}>Maloo test results and logs.</a>", false)
-            }
-        }
-    }
-}
+                summary.appendText("See <a href=${{buildlink}}>build job.</a>", false)
+                summary.appendText("<br>See <a href=${{maloolink}}>Maloo test results and logs.</a>", false)
+            }}
+        }}
+    }}
+}}
