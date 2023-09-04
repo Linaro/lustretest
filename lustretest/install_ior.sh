8742ebdbe712fe1f9431863b3bc3f92063a1f2ee
Xinliang Liu <xinliang.liu@linaro.org>
2023-09-04 01:14:24 +0000

    Install openmpi for mdtest
    Signed-off-by: Xinliang Liu <xinliang.liu@linaro.org>

--- lustretest/install_ior.sh
+++ lustretest/install_ior.sh
@@ -3,7 +3,7 @@
 pushd ~
 command -v mdtest && exit 0 || "Install ior and mdtest ..."
 sudo dnf install -y git ccache gcc make autoconf \
-	automake libtool openmpi-devel &&
+	automake libtool openmpi openmpi-devel &&
 module load mpi/openmpi-$(arch) &&
 git clone https://github.com/hpc/ior.git &&
 cd ior/ &&
